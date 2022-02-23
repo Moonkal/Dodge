be0a7c7a71cde8ab54e68881daf4b9fd6927fc05
Moonkal <mp4608@naver.com>
2022-02-23 17:26:35 +0900

    20220223
    닷지 게임

---
+++ dodge/Assets/Player_move.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody player;
+    public float speed = 8f;
+
+/* 이동의 사용할 컴포넌트*/
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
